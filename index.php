382847fd2a7c0ccbf8f7b36a8d24e015d43ce6c2
Sathvik Chereddy <56492353+Sathware@users.noreply.github.com>
2021-10-05 12:41:30 -0400

    Add project files.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<head>
+    <title>Scry</title>
+</head>
+
+<body>
+    <h1>This is Scry</h1>
+</body>
+
+<?php
+echo "This is php";
+?>
+
+<!-- Bubble Canvas -->
+<script src="https://cdn.jsdelivr.net/npm/bubbly-bg@1.0.0/dist/bubbly-bg.js"></script>
+<script>bubbly();</script>
