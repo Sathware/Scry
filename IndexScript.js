810a50549a31a34c844e05bcc2088536b91dbc3f
huntaj2 <91857795+huntaj2@users.noreply.github.com>
2021-10-17 15:27:07 -0400

    Add files via upload
    Created 12 apps with names and pictures. All of these apps are available on Apple app store and the pictures for each listing are in the AppLogos.zip folder

---
+++ IndexScript.js
@@ -0,0 +1,29 @@
+function userLogin() {
+    let loginForm = document.forms["userCredentials"];
+    let uName = loginForm["username"].value;
+    let pWord = loginForm["password"].value;
+    if (uName == uName && pWord == "pwd") {
+        let UserDisplay = document.getElementById("userDisplay");
+        UserDisplay.innerHTML = uName;
+        UserDisplay.style.visibility = "visible";
+        loginForm.style.visibility = "hidden";
+    }
+    else {
+        alert("Invalid Credentials");
+    }
+}
+
+function filterSearch() {
+    let input = document.getElementById("searchbar");
+    let filter = input.value.toUpperCase();
+    let AppListings = document.getElementsByClassName("applisting");
+    for (i = 0; i < AppListings.length; i++) {
+        let x = AppListings[i].getAttribute("title").toUpperCase();
+        if (x.includes(filter)) {
+            AppListings[i].style.display = "block";
+        }
+        else {
+            AppListings[i].style.display = "none";
+        }
+    }
+}
