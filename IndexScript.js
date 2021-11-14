f0d436383bfeeb3df7aa37fc9e971a27c2703857
Sathvik Chereddy <56492353+Sathware@users.noreply.github.com>
2021-11-14 17:00:59 -0500

    Got comments working

--- IndexScript.js
+++ IndexScript.js
@@ -1,72 +1,3 @@
-function sortBy() {
-    let choice = options.value;
-    let AppListings = document.getElementsByClassName("applisting");
-    let arrayToSort = new Array()
-    for (var i=0;i<AppListings.length;i++) {
-        let name = AppListings[i].getAttribute("title");
-        let type = AppListings[i].className;
-        let content = AppListings[i].innerText;
-        //let category = AppListings[i].getAttribute("category");
-        arrayToSort[i] = [name, type, content];//, prices, category, headers.innerText];
-    }
-
-    if (choice == "alphabetical")
-    {
-        arrayToSort.sort(function(a,b){return a[0].localeCompare(b[0]);});
-        setAppListings(arrayToSort, AppListings);
-    }
-    else if (choice == "category") 
-    {
-       arrayToSort.sort(function(a,b){return a[1].localeCompare(b[1]);});
-       setAppListings(arrayToSort, AppListings);
-    }
-
-    //     for (var i=0;i<arrayToSort.length;i++) {       
-    //         AppListings[i].setAttribute("title", arrayToSort[i][0]);
-    //         AppListings[i].innerHTML = arrayToSort[i][2];
-    //     }
-    // }
-}
-
-function setAppListings(sorted, applistings)
-{
-    for (var i=0;i<sorted.length;i++) {       
-        applistings[i].setAttribute("title", sorted[i][0]);
-        applistings[i].setAttribute("class", sorted[i][1]);
-        applistings[i].innerText = sorted[i][2];
-    }
-}
-
-function search()
-{
-    let input = document.getElementById("searchbar");
-    let filter = input.value.toUpperCase();
-    let AppListings = document.getElementsByClassName("applisting");
-
-    for( i = 0; i < AppListings.length; i++)
-    {
-        let x = AppListings[i].getAttribute("title").toUpperCase();
-        if (x.substring(0, filter.length).includes(filter))
-        {
-            AppListings[i].style.display = "block";
-        }
-        else
-        {
-            AppListings[i].style.display = "none";
-        }
-    }
-
-    // let section = document.getElementById(filter.toLowerCase().charAt(0));
-    // section.style.display = 'none';
-}
-
-function showSignIn()
-{
-    document.getElementById("overlay").style.display = "block";
-    document.getElementById("signin").style.display = "block";
-    document.body.style.overflow = "hidden";
-}
-
 function validateCredentials()
 {
     let userInfo = document.forms["user_verification"];
@@ -79,61 +10,6 @@ function validateCredentials()
     {
         alert("Please enter a valid username and password.");
         return false;
-    }
-}
-
-function showData(appListing)//In Progress, need to figure out price and categories
-{
-    let AppData = document.getElementById("appdata");
-    appName = appListing.getElementsByTagName("h3")[0].innerText;
-    AppData.getElementsByTagName("h1")[0].innerText = appName;
-    AppData.getElementsByTagName("img")[0].setAttribute("src", appListing.getElementsByTagName("img")[0].getAttribute("src"));
-    document.getElementById("overlay").style.display = "block";
-    AppData.style.display = "block";
-    AppData.getElementsByTagName("p")[0].innerText = appListing.getAttribute("description");
-    document.body.style.overflow = "hidden";
-    showComments(appName);
-}
-
-//!!!!!!! CHANGE URL
-function showComments(appName)
-{
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("comments").innerHTML = this.responseText;
-      }
-    };
-    let params = "appname="+appName+"&action=show";
-    xmlhttp.open("POST", "comment.php", true);
-    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xmlhttp.send(params);
-}
-
-function makeComment(modal)
-{
-    let comment = document.getElementById("commentfield");
-    let appName = modal.getElementsByTagName("h1")[0].innerText;
-    let userName = document.getElementById("userDisplay").innerText;
-    if (comment != null)
-    {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                if (this.responseText == "error")
-                {
-                    alert("Your comment could not be made. Try Again Later.");
-                }
-                else
-                {
-                    document.getElementById("comments").innerHTML += "<tr><td>"+userName+": </td><td> "+comment.value+"</td></tr>";
-                }
-            }
-        };
-        let params = "appname=" + appName + "&action=make&comment="+comment.value+"&username="+userName;
-        xmlhttp.open("POST", "comment.php", true);
-        xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        xmlhttp.send(params);
     }
 }
 
@@ -151,9 +27,9 @@ function dismiss(e)
     let x = e.currentTarget.id;
     if (x.valueOf() == "overlay")
     {
-        document.getElementById("overlay").style.display = "none";
-        document.getElementById("signin").style.display = "none";
-        document.getElementById("appdata").style.display = "none";
+        document.getElementById("overlay").classList.remove("active");
+        document.getElementById("signin").classList.remove("active");
+        document.getElementById("appdata").classList.remove("active");
         document.body.style.overflow = "auto";
     }
 }
@@ -161,5 +37,9 @@ function dismiss(e)
 document.addEventListener("DOMContentLoaded", function() {
     //The first argument are the elements to which the plugin shall be initialized
     //The second argument has to be at least a empty object or a object with your desired options
-    OverlayScrollbars(document.querySelectorAll('body'), { });
+    OverlayScrollbars(document.querySelectorAll('body'), { 
+        overflowBehavior : {
+            x : "hidden"
+        }
+    });
 });
--- user.php
+++ user.php
@@ -1,17 +1,19 @@
 <?php
     //Connects to the specified database and verifies user info, and sets the appropriate flags whether a username, password, or both are invalid
-    function login(&$database, &$flagErrors, &$userInputtedName, &$userInputtedPass)
+    function login(&$database, &$flagErrors, &$userInputtedName, &$userInputtedPass, &$usertype)
     {
-        $query = $database->prepare("SELECT pass FROM user WHERE name = ?;"); //$conn is the global variabel for the connection to the database scry
+        $query = $database->prepare("SELECT pass, user.type FROM user WHERE name = ?;"); //$conn is the global variabel for the connection to the database scry
         $query->bind_param("s", $userInputtedName); //$query will take $username as variable for the aove '?' with the type of $username as String specified by the "s"
         $query->execute();
-        $query->bind_result($storedPass); //Will create a new local variable $pass and store the value from database in that variable
+        $query->bind_result($storedPass, $usertype); //Will create a new local variable $pass and store the value from database in that variable
 
         if ($query->fetch())
         {
             if (password_verify($userInputtedPass, $storedPass)) //Checks that the password inputted by the user and stored in the database are the same
             {
-                echo $userInputtedName;
+                $query->close();
+                pushUserCookie($userInputtedName);
+                return true; //Deallocates handle so following queries can be executed
             }
             else
             {
@@ -26,14 +28,15 @@
             $query->close();
             return false;
         }
-
-        $query->close();//Deallocates handle so following queries can be executed
-        //echo "<span id='inputtedPass' style='display:none;'>".$userInputtedPass."</span>";
-        return true;
     }
 
     function signup(&$database, &$flagErrors, &$userInputtedName, &$userInputtedPass)
     {
+        if ($userInputtedName == "" || $userInputtedName == "")
+        {
+            $flagErrors .= "Username or password cannot be empty";
+            return false;
+        }
         $query = $database->prepare("INSERT INTO user VALUES (?, ?, 'user');");
         $hashedPassword = password_hash($userInputtedPass, PASSWORD_DEFAULT);
         $query->bind_param("ss", $userInputtedName, $hashedPassword);
@@ -41,7 +44,9 @@
 
         if ($query->error == "")
         {
-            echo $userInputtedName;
+            $query->close();//deallocates handle so further queries can be executed
+            pushUserCookie($userInputtedName);
+            return true;
         }
         else
         {
@@ -49,15 +54,12 @@
             $query->close();
             return false;
         }
-
-        $query->close();
-        return true;
     }
 
-    //If there are any errors, then css should display
-    function displayIfError(&$flagErrors)
+    //Saves a cookie of the user's encryptes username so user security is maintained
+    function pushUserCookie($userName)
     {
-        echo ($flagErrors != "") ? "display:block;": "";
+        setcookie("user", password_hash($userName, PASSWORD_DEFAULT), time() + (86400 * 3), "/"); // 86400 = 1 day
     }
 
 ?>
